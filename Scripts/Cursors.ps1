47e868ef0658873a2161d84e747b87234cc65401
Dmitry Nefedov <10544660+farag2@users.noreply.github.com>
2026-04-24 00:09:57 +0300

    Update Cursors.ps1

--- Scripts/Cursors.ps1
+++ Scripts/Cursors.ps1
@@ -3,6 +3,7 @@
 
 # Get access token
 # https://www.deviantart.com/developers/authentication
+
 $Body = @{
 	grant_type    = "client_credentials"
 	client_id     = $env:DEVIANTART_CLIENT_ID
@@ -11,6 +12,7 @@ $Body = @{
 $Parameters = @{
 	Uri             = "https://www.deviantart.com/oauth2/token"
 	Body            = $Body
+	Method          = "Post"
 	Verbose         = $true
 	UseBasicParsing = $true
 }
@@ -19,12 +21,12 @@ $Response = Invoke-RestMethod @Parameters
 # Get download URL
 # https://www.deviantart.com/developers/http/v1/20240701/deviation_download/bed6982b88949bdb08b52cd6763fcafd
 # UUID is 8A8DC033-242C-DD2E-EDB0-CC864772D5F4
-$Body = @{
-	access_token = $Response.access_token
+$Headers = @{
+	Authorization = "Bearer $($Response.access_token)"
 }
 $Parameters = @{
 	Uri             = "https://www.deviantart.com/api/v1/oauth2/deviation/download/8A8DC033-242C-DD2E-EDB0-CC864772D5F4"
-	Body            = $Body
+	Headers         = $Headers
 	Verbose         = $true
 	UseBasicParsing = $true
 }
